2fa094c8d222efac527a0cada3be75283e1052e7
NeejaViju <neejaviju22@gmail.com>
2023-09-02 11:49:11 +0530

    reduced some codes on cases sensitives

--- validation.py
+++ validation.py
@@ -4,6 +4,8 @@ import json
 import pandas as pd
 
 
+
+
 # Path to the parent directory containing student folders
 parent_folder_path = "Students"
 
@@ -44,25 +46,22 @@ student_data = {
 with open("expected_files_config.json", "r") as config_file:
     expected_files_config = json.load(config_file)
 
-# Function to validate week folder contents
+# New function for checking file presence
+def is_file_present(expected_file, files_in_folder):
+    return any(expected_file.lower() == file_in_folder.lower() for file_in_folder in files_in_folder)
+
+# Updated function to validate week folder contents
 def validate_week_folder(week_folder_path, expected_files):
     files_in_folder = os.listdir(week_folder_path)
-    files_in_folder_stripped = [f.strip() for f in files_in_folder]
-    present_files = [file for file in expected_files if any(file.lower() == f.lower() for f in files_in_folder_stripped)]
-    missing_files = [file for file in expected_files if not any(file.lower() == f.lower() for f in files_in_folder_stripped)]
+    files_in_folder_stripped = [os.path.splitext(f)[0].strip().lower() for f in files_in_folder]
+    present_files = [file for file in expected_files if is_file_present(file, files_in_folder_stripped)]
+    missing_files = [file for file in expected_files if not is_file_present(file, files_in_folder_stripped)]
     return present_files, missing_files
 
-
-# ... (previous part of your code)
-
-# Specific week you want to check
-specific_week = "Week02"
-
-# Get current date and time
+specific_week = "Week01"
 current_datetime = datetime.datetime.now()
-current_datetime_str = current_datetime.strftime("%Y-%m-%d %I:%M:%S %p")  # Format with AM/PM
+current_datetime_str = current_datetime.strftime("%Y-%m-%d %I:%M:%S %p")
 
-# Create a list to store report data
 report_data = []
 
 for student_id, student_name in student_data.items():
@@ -74,30 +73,20 @@ for student_id, student_name in student_data.items():
         expected_files = expected_files_config.get(week_folder_name, {}).get(student_id, [])
         present_files, missing_files = validate_week_folder(week_folder_path, expected_files)
         missing_files_str = ', '.join(missing_files)
-        if len(present_files) == len(expected_files):
-            completion_status = "Completed"
-        else:
-            completion_status = "Pending"
+        completion_status = "Completed" if len(present_files) == len(expected_files) else "Pending"
         report_data.append([student_id, student_name, week_folder_name, missing_files_str, completion_status])
     else:
-        report_data.append([student_id, student_name, week_folder_name, "Folder or data not found", "", ""])
+        report_data.append([student_id, student_name, week_folder_name, "Folder or data not found", ""])
 
-# Convert the report_data list to a pandas DataFrame
 report_df = pd.DataFrame(report_data, columns=["Student ID", "Student Name", "Week", "Pending Task", "Completion Status"])
 
-# Save DataFrame to an Excel file
 report_excel_filename = f"{specific_week}_report.xlsx"
-
-# Create a Pandas Excel writer using XlsxWriter as the engine
 with pd.ExcelWriter(report_excel_filename, engine='xlsxwriter') as writer:
-    # Write the DataFrame data to XlsxWriter
     report_df.to_excel(writer, sheet_name='Report', index=False)
     
-    # Get the xlsxwriter workbook and worksheet objects
     workbook = writer.book
     worksheet = writer.sheets['Report']
 
-    # Add a header format.
     header_format = workbook.add_format({
         'bold': True,
         'text_wrap': True,
@@ -107,27 +96,19 @@ with pd.ExcelWriter(report_excel_filename, engine='xlsxwriter') as writer:
         'border': 1
     })
 
-    # Set the column widths and apply the header format.
     for col_num, value in enumerate(report_df.columns.values):
         worksheet.write(0, col_num, value, header_format)
         column_len = max(report_df[value].astype(str).apply(len).max(), len(value))
         col_width = column_len + 2
         worksheet.set_column(col_num, col_num, col_width)
     
-    # Add the specific week to the worksheet
     worksheet.write(len(report_df) + 2, 0, f'Week: {specific_week}')
-    
-    # Add the current date and time to the worksheet
     worksheet.write(len(report_df) + 3, 0, f'Generated: {current_datetime_str}')
     
-    # Add cell formats for completed students' names and statuses (green background, white text)
     green_format = workbook.add_format({'bg_color': 'green', 'font_color': 'white', 'bold': True})
-
-    # Iterate through the DataFrame to format cells based on completion status
     for row_num, completion_status in enumerate(report_df["Completion Status"]):
         if completion_status == "Completed":
             worksheet.write(row_num + 1, 1, report_df.at[row_num, "Student Name"], green_format)
             worksheet.write(row_num + 1, 4, completion_status, green_format)
     
-# Print a message indicating Excel report generation
 print(f"Excel report generated: {report_excel_filename}")
